937d5c418ac44dc5d8dbdc0ca61571d313475cb6
Clutch-Hinkle <chris.hinkle1999@knights.ucf.edu>
2023-03-15 18:01:04 -0400

    initial commit

---
+++ Chromo.java
@@ -0,0 +1,162 @@
+/******************************************************************************
+*  A Teaching GA					  Developed by Hal Stringer & Annie Wu, UCF
+*  Version 2, January 18, 2004
+*******************************************************************************/
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+
+
+public class Chromo
+{
+/*******************************************************************************
+*                            INSTANCE VARIABLES                                *
+*******************************************************************************/
+
+	public int[] chromo;
+	public double rawFitness;
+	public double sclFitness;
+	public double proFitness;
+
+/*******************************************************************************
+*                            INSTANCE VARIABLES                                *
+*******************************************************************************/
+
+private static double randnum;
+
+/*******************************************************************************
+*                              CONSTRUCTORS                                    *
+*******************************************************************************/
+
+	public Chromo(){
+
+		int[] tmp = new int[48];
+		for (int i = 0; i < 48; i++)
+		{
+			tmp[i] = i;
+		}
+		
+		//Doesnt work :(
+		Collections.shuffle(Arrays.asList(tmp));
+		chromo = tmp;
+
+
+		this.rawFitness = -1;   //  Fitness not yet evaluated
+		this.sclFitness = -1;   //  Fitness not yet scaled
+		this.proFitness = -1;   //  Fitness not yet proportionalized
+	}
+
+
+/*******************************************************************************
+*                                MEMBER METHODS                                *
+*******************************************************************************/
+
+
+	//  Mutate a Chromosome Based on Mutation Type *****************************
+
+	public void doMutation(){
+
+		String mutChromo = "";
+		char x;
+
+		switch (Parameters.mutationType){
+
+		case 1:     //  Replace with new random number
+
+			break;
+
+		default:
+			System.out.println("ERROR - No mutation method selected");
+		}
+	}
+
+/*******************************************************************************
+*                             STATIC METHODS                                   *
+*******************************************************************************/
+
+	//  Select a parent for crossover ******************************************
+
+	public static int selectParent(){
+
+		double rWheel = 0;
+		int j = 0;
+		int k = 0;
+
+		switch (Parameters.selectType){
+
+		case 1:     // Proportional Selection
+			randnum = Search.r.nextDouble();
+			for (j=0; j<Parameters.popSize; j++){
+				rWheel = rWheel + Search.member[j].proFitness;
+				if (randnum < rWheel) return(j);
+			}
+			break;
+
+		case 3:     // Random Selection
+			randnum = Search.r.nextDouble();
+			j = (int) (randnum * Parameters.popSize);
+			return(j);
+
+		case 2:     //  Tournament Selection
+
+		default:
+			System.out.println("ERROR - No selection method selected");
+		}
+	return(-1);
+	}
+
+	//  Produce a new child from two parents  **********************************
+
+	public static void mateParents(int pnum1, int pnum2, Chromo parent1, Chromo parent2, Chromo child1, Chromo child2){
+
+		int xoverPoint1;
+		int xoverPoint2;
+
+		switch (Parameters.xoverType){
+
+		case 1:     //  Single Point Crossover
+
+		case 2:     //  Two Point Crossover
+
+		case 3:     //  Uniform Crossover
+
+		default:
+			System.out.println("ERROR - Bad crossover method selected");
+		}
+
+		//  Set fitness values back to zero
+		child1.rawFitness = -1;   //  Fitness not yet evaluated
+		child1.sclFitness = -1;   //  Fitness not yet scaled
+		child1.proFitness = -1;   //  Fitness not yet proportionalized
+		child2.rawFitness = -1;   //  Fitness not yet evaluated
+		child2.sclFitness = -1;   //  Fitness not yet scaled
+		child2.proFitness = -1;   //  Fitness not yet proportionalized
+	}
+
+	//  Produce a new child from a single parent  ******************************
+
+	public static void mateParents(int pnum, Chromo parent, Chromo child){
+
+		//  Create child chromosome from parental material
+		child.chromo = parent.chromo;
+
+		//  Set fitness values back to zero
+		child.rawFitness = -1;   //  Fitness not yet evaluated
+		child.sclFitness = -1;   //  Fitness not yet scaled
+		child.proFitness = -1;   //  Fitness not yet proportionalized
+	}
+
+	//  Copy one chromosome to another  ***************************************
+
+	public static void copyB2A (Chromo targetA, Chromo sourceB){
+
+		targetA.chromo = sourceB.chromo;
+
+		targetA.rawFitness = sourceB.rawFitness;
+		targetA.sclFitness = sourceB.sclFitness;
+		targetA.proFitness = sourceB.proFitness;
+		return;
+	}
+
+}   // End of Chromo.java ******************************************************
---
+++ TSP.java
@@ -0,0 +1,76 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+
+public class TSP extends FitnessFunction{
+
+    double[][] cityArray; 
+
+/*******************************************************************************
+*                              HELPER CLASSES                                  *
+*******************************************************************************/
+
+    private class Point {
+        double Lat, Lon;
+
+        public Point(double x, double y) {
+            this.Lat = x * (Math.PI / 180);
+            this.Lon = y * (Math.PI / 180);
+        }
+
+        public double dist(Point p) {
+            // Distance formula with Lat and Long
+            // 3963.0 * arccos[(sin(lat1) * sin(lat2)) + cos(lat1) * cos(lat2) * cos(long2 – long1)]
+            return  3963.0 * Math.acos((Math.sin(Lat)* Math.sin(p.Lat)) + (Math.cos(Lat) * Math.cos(p.Lat) * Math.cos(p.Lon - Lon)));
+        }
+    }
+
+/*******************************************************************************
+*                            STATIC VARIABLES                                  *
+*******************************************************************************/
+
+/*******************************************************************************
+*                              CONSTRUCTORS                                    *
+*******************************************************************************/
+
+	public TSP(double[][] city) {
+		name = "TSP problem";
+        this.cityArray = city;
+	}
+
+//  COMPUTE A CHROMOSOME'S RAW FITNESS *************************************
+
+	public void doRawFitness(Chromo X){
+        // Assumes no more than 100 points
+        int fitness = 0;
+
+        //For loop adds the distance between each city in the chromosome
+        for (int i=0; i<47; i++)
+        {
+            int city1 = X.chromo[i];
+            int city2 = X.chromo[i+1];
+            Point p1 = new Point(cityArray[city1][0],cityArray[city1][1]);
+            Point p2 = new Point(cityArray[city2][0],cityArray[city2][1]);
+
+            fitness += p1.dist(p2);
+        }
+
+        // Add the distance from first city to last city since its a cycle
+        int city1 = X.chromo[47];
+        int city2 = X.chromo[0];
+        Point p1 = new Point(cityArray[city1][0],cityArray[city1][1]);
+        Point p2 = new Point(cityArray[city2][0],cityArray[city2][1]);
+
+        fitness += p1.dist(p2);
+
+        X.rawFitness = fitness;
+
+	}
+
+//  PRINT OUT AN INDIVIDUAL GENE TO THE SUMMARY FILE *********************************
+
+	public void doPrintGenes(Chromo X, FileWriter output) throws java.io.IOException{
+
+		return;
+	}
+}
