723dc04e0d1bed5708f3e04359d580e79349b65e
Pierce <piercepowell94@knights.ucf.edu>
2023-03-16 20:38:05 -0400

    forgot to remove a print

--- Chromo.java
+++ Chromo.java
@@ -47,9 +47,6 @@ private static double randnum;
 
 		int[] x = intList.stream().mapToInt(i->i).toArray();
 
-		for (int i = 0; i < x.length; i++)
-			System.out.print(x[i] + " ");
-
 		System.out.println("\n\n\n");
 
 		this.chromo = x;
